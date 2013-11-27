aab5121581c7d7161561da74f301f014e9942b09
noko <nokok.kz@gmail.com>
2013-11-27 09:50:10 +0900

    Implement blank window

--- src/main/java/net/nokok/twitduke/gui/MainWindow.java
+++ src/main/java/net/nokok/twitduke/gui/MainWindow.java
@@ -1,8 +1,28 @@
 package net.nokok.twitduke.gui;
 
+import net.nokok.twitduke.util.config.DefaultConfig;
+
+import javax.swing.*;
+
 /**
  * @author noko
  *         TwitDukeのメインウィンドウのクラスです
  */
-public class MainWindow {
+public class MainWindow extends JFrame {
+
+    private MainWindow() {
+        this.setSize(DefaultConfig.WINDOW_SIZE);
+        this.setTitle(DefaultConfig.FORMATTED_APP_VERSION);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private static class SingletonHolder {
+        private static final MainWindow instance = new MainWindow();
+    }
+
+    public static MainWindow getInstance() {
+        return SingletonHolder.instance;
+    }
+
 }
--- src/main/java/net/nokok/twitduke/main/Main.java
+++ src/main/java/net/nokok/twitduke/main/Main.java
@@ -1,12 +1,14 @@
 package net.nokok.twitduke.main;
 
 
+import net.nokok.twitduke.gui.MainWindow;
+
 /**
  * @author noko
  *         TwitDukeメインクラス
  */
 public class Main {
     public static void main(String[] args) {
-
+        MainWindow.getInstance();
     }
 }
