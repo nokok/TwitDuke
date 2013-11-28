55ba5ac41dcd369eac0f691c69b6b1324e3109bc
nokok <nokok.kz@gmail.com>
2013-11-29 08:46:14 +0900

    Update class TweetList.java
    apply singleton

--- src/main/java/net/nokok/twitduke/gui/TweetList.java
+++ src/main/java/net/nokok/twitduke/gui/TweetList.java
@@ -1,9 +1,20 @@
 package net.nokok.twitduke.gui;
 
+import net.nokok.twitduke.util.config.DefaultConfig;
+
 /**
  * @author noko
  *         ツイートのリストを保持するクラスです
  *         このリストにTweetCellが追加されてタイムラインを表示します
  */
 public class TweetList extends ElementList {
+    private static final TweetList instance = new TweetList();
+
+    private TweetList() {
+        this.setBackground(DefaultConfig.TWEETLIST_COLOR);
+    }
+
+    public static TweetList getInstance() {
+        return instance;
+    }
 }
