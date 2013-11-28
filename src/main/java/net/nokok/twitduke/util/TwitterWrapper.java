4a6370a42181e20f3f3269987350dd4b4b87f948
nokok <nokok.kz@gmail.com>
2013-11-29 01:56:27 +0900

    Apply singleton TwitterWrapper.java

--- src/main/java/net/nokok/twitduke/util/TwitterWrapper.java
+++ src/main/java/net/nokok/twitduke/util/TwitterWrapper.java
@@ -5,4 +5,15 @@ package net.nokok.twitduke.util;
  *         Twitter4jのラッパクラスです
  */
 public class TwitterWrapper {
+
+    private static final TwitterWrapper instance = new TwitterWrapper();
+
+    private TwitterWrapper() {
+
+    }
+
+    public static TwitterWrapper getInstance() {
+        return instance;
+    }
+
 }
