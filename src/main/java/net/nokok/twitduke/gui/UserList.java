e2d96866c476066f45c845b74cac2b8e2761869b
nokok <nokok.kz@gmail.com>
2013-11-29 08:47:10 +0900

    Update UserList.java
    apply singleton

--- src/main/java/net/nokok/twitduke/gui/UserList.java
+++ src/main/java/net/nokok/twitduke/gui/UserList.java
@@ -5,4 +5,13 @@ package net.nokok.twitduke.gui;
  *         ユーザーのリストを保持するクラスです
  */
 public class UserList extends ElementList {
+    private static final UserList instance = new UserList();
+
+    private UserList() {
+
+    }
+
+    public UserList getInstance() {
+        return instance;
+    }
 }
