12e84c3e53fec3a505be56e98ed18592fb7ad89b
nokok <nokok.kz@gmail.com>
2013-11-29 01:57:56 +0900

    Update MainWindow.java

--- src/main/java/net/nokok/twitduke/gui/MainWindow.java
+++ src/main/java/net/nokok/twitduke/gui/MainWindow.java
@@ -3,6 +3,7 @@ package net.nokok.twitduke.gui;
 import net.nokok.twitduke.util.config.DefaultConfig;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author noko
@@ -10,11 +11,30 @@ import javax.swing.*;
  */
 public class MainWindow extends JFrame {
 
+    private final JTextArea tweetTextArea = new JTextArea();
+
     private MainWindow() {
         this.setSize(DefaultConfig.WINDOW_SIZE);
-        this.setTitle(DefaultConfig.FORMATTED_APP_VERSION);
+        this.setTitle(DefaultConfig.APP_TITLE + DefaultConfig.FORMATTED_APP_VERSION);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        initUI();
         this.setVisible(true);
+    }
+
+    private void initUI() {
+        BorderLayout borderLayout = new BorderLayout();
+        this.setLayout(borderLayout);
+
+        JPanel topPanel = new JPanel(new BorderLayout());
+        tweetTextArea.setPreferredSize(DefaultConfig.TWEET_TEXTAREA_SIZE);
+        tweetTextArea.setLineWrap(true);
+        topPanel.add(tweetTextArea, BorderLayout.NORTH);
+        topPanel.add(new Toolbar(), BorderLayout.SOUTH);
+        this.add(topPanel, BorderLayout.NORTH);
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(TweetList.getInstance(), BorderLayout.CENTER);
+        this.add(centerPanel, BorderLayout.CENTER);
+
     }
 
     private static class SingletonHolder {
@@ -24,5 +44,4 @@ public class MainWindow extends JFrame {
     public static MainWindow getInstance() {
         return SingletonHolder.instance;
     }
-
 }
