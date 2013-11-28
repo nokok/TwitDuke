ee66ce25071081aefb9088482671ddccf73d796b
nokok <nokok.kz@gmail.com>
2013-11-29 08:45:32 +0900

    Update ElementCell.java

--- src/main/java/net/nokok/twitduke/gui/ElementCell.java
+++ src/main/java/net/nokok/twitduke/gui/ElementCell.java
@@ -1,5 +1,7 @@
 package net.nokok.twitduke.gui;
 
+import net.nokok.twitduke.util.config.DefaultConfig;
+
 import javax.swing.*;
 
 /**
@@ -7,4 +9,8 @@ import javax.swing.*;
  *         TweetListやUserListなどに使用されるセルの共通動作を定義します
  */
 public class ElementCell extends JPanel {
+
+    public ElementCell() {
+        this.setSize(DefaultConfig.CELL_SIZE);
+    }
 }
