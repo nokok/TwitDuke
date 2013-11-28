2ec9aa0f961b0595a6dcea570ca3c7cfae8409b8
nokok <nokok.kz@gmail.com>
2013-11-28 23:49:19 +0900

    Update Toolbar.java
    ボタンのアクションリスナのコードを追加
    設定ボタンで設定画面のフレームが出てくるように変更
    他のボタンは他のクラスの実装待ち

--- src/main/java/net/nokok/twitduke/gui/SettingsWindow.java
+++ src/main/java/net/nokok/twitduke/gui/SettingsWindow.java
@@ -1,8 +1,22 @@
 package net.nokok.twitduke.gui;
 
+import net.nokok.twitduke.util.config.DefaultConfig;
+
+import javax.swing.*;
+
 /**
  * @author noko
  *         設定画面のクラスです
  */
-public class SettingsWindow {
+public class SettingsWindow extends JFrame {
+    private static final SettingsWindow instance = new SettingsWindow();
+
+    private SettingsWindow() {
+        this.setSize(DefaultConfig.WINDOW_SIZE);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+    public static SettingsWindow getInstance() {
+        return instance;
+    }
 }
--- src/main/java/net/nokok/twitduke/gui/Toolbar.java
+++ src/main/java/net/nokok/twitduke/gui/Toolbar.java
@@ -4,6 +4,8 @@ import net.nokok.twitduke.util.config.DefaultConfig;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author noko
@@ -16,9 +18,42 @@ public class Toolbar extends JPanel {
 
         this.setSize(DefaultConfig.TOOLBAR_SIZE);
 
-        this.add(new ToolButton("設定"));
-        this.add(new ToolButton("更新"));
-        this.add(new ToolButton("ユーザー切り替え"));
-        this.add(new ToolButton("送信"));
+        //TODO:Java8がリリースされたらラムダで書きなおす
+
+        ToolButton settingsButton = new ToolButton("設定");
+        settingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SettingsWindow.getInstance().setVisible(true);
+            }
+        });
+        this.add(settingsButton);
+
+        ToolButton refreshButton = new ToolButton("更新");
+        refreshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO:Twitterのラッパの実装が終わったらコーディングする
+            }
+        });
+        this.add(refreshButton);
+
+        ToolButton userSwitcher = new ToolButton("ユーザー");
+        userSwitcher.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO:複数ユーザーに対応したらコーディングする
+            }
+        });
+        this.add(userSwitcher);
+
+        ToolButton sendButton = new ToolButton("送信");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO:Twitterのラッパの実装が終わったらコーディングする
+            }
+        });
+        this.add(sendButton);
     }
 }
