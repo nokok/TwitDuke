2ecff5980ce8cd70534f4ff958b283717c90e158
noko <nokok.kz@gmail.com>
2013-11-30 11:36:54 +0900

    Update TwitterWrapper
    apply singleton
    add sendTweet method

--- src/main/java/net/nokok/twitduke/util/TwitterWrapper.java
+++ src/main/java/net/nokok/twitduke/util/TwitterWrapper.java
@@ -1,15 +1,20 @@
 package net.nokok.twitduke.util;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 /**
  * @author noko
  *         Twitter4jのラッパクラスです
  */
 public class TwitterWrapper {
 
+    private Twitter twitter;
     private static final TwitterWrapper instance = new TwitterWrapper();
 
     private TwitterWrapper() {
-
+        twitter = new TwitterFactory().getInstance();
     }
 
     public static TwitterWrapper getInstance() {
@@ -17,7 +22,11 @@ public class TwitterWrapper {
     }
 
     public void sendTweet(String tweet) {
-
+        try {
+            twitter.updateStatus(tweet);
+        } catch (TwitterException e) {
+            Log.log("Twitterへのポストに失敗しました");
+        }
     }
 
 }
