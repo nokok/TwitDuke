523b8b24e2d6fbf7ff76dd78fef6694c510d0075
noko <nokok.kz@gmail.com>
2013-11-30 17:34:22 +0900

    Update DefaultConfig.java
    ブランク行の削除
    フィールドの整列

--- src/main/java/net/nokok/twitduke/util/config/DefaultConfig.java
+++ src/main/java/net/nokok/twitduke/util/config/DefaultConfig.java
@@ -10,28 +10,24 @@ public enum DefaultConfig {
     INSTANCE;
 
     /* OAuth Keys */
-    public static final String OAUTH_CONSUMER_KEY = "";
-    public static final String OAUTH_CONSUMER_SECRET = "";
-
+    public static final String    OAUTH_CONSUMER_KEY      = "";
+    public static final String    OAUTH_CONSUMER_SECRET   = "";
     /* GUI Settings */
-    public static final Dimension WINDOW_SIZE = new Dimension(512, 640);
-    public static final String APP_TITLE = "TwitDuke";
-    public static final String APP_VERSION = "Alpha";
-    public static final Long REVISION = new Long(1000);
-    public static final String FORMATTED_APP_VERSION = APP_VERSION + " (Rev." + REVISION + ")";
-
+    public static final Dimension WINDOW_SIZE             = new Dimension(512, 640);
+    public static final String    APP_TITLE               = "TwitDuke";
+    public static final String    APP_VERSION             = "Alpha";
+    public static final Long      REVISION                = new Long(1000);
+    public static final String    FORMATTED_APP_VERSION   = APP_VERSION + " (Rev." + REVISION + ")";
     /* ToolBar */
-    public static final Dimension CELL_SIZE = new Dimension(512, 50);
-    public static final Dimension TOOLBAR_SIZE = new Dimension(512, 40);
-    public static final Dimension TOOLBAR_BUTTON_SIZE = new Dimension(70, 30);
-
+    public static final Dimension CELL_SIZE               = new Dimension(512, 50);
+    public static final Dimension TOOLBAR_SIZE            = new Dimension(512, 40);
+    public static final Dimension TOOLBAR_BUTTON_SIZE     = new Dimension(70, 30);
     /* Color */
-    public static final Color TWEETLIST_COLOR = new Color(189, 195, 199);
-    public static final Color BACKGROUND_BUTTON_COLOR = new Color(44, 62, 80);
-    public static final Color FOREGROUND_BUTTON_COLOR = new Color(236, 240, 241);
-
+    public static final Color     TWEETLIST_COLOR         = new Color(189, 195, 199);
+    public static final Color     BACKGROUND_BUTTON_COLOR = new Color(44, 62, 80);
+    public static final Color     FOREGROUND_BUTTON_COLOR = new Color(236, 240, 241);
     /* Others */
-    public static final Dimension TWEET_OPTION_ICON_SIZE = new Dimension(24, 24);
-    public static final Dimension TWEET_TEXTAREA_SIZE = new Dimension(512, 45);
+    public static final Dimension TWEET_OPTION_ICON_SIZE  = new Dimension(24, 24);
+    public static final Dimension TWEET_TEXTAREA_SIZE     = new Dimension(512, 45);
 
 }
