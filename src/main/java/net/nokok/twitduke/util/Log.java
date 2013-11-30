87d40acbd6da823604c943d4a4052ca30da7c146
noko <nokok.kz@gmail.com>
2013-11-30 11:36:27 +0900

    Add class Log.java

---
+++ src/main/java/net/nokok/twitduke/util/Log.java
@@ -0,0 +1,18 @@
+package net.nokok.twitduke.util;
+
+public class Log {
+    private static final Log instance = new Log();
+    private static StringBuilder builder;
+
+    private Log() {
+        builder = new StringBuilder();
+    }
+
+    public static Log getInstance() {
+        return instance;
+    }
+
+    public static void log(String text) {
+        builder.append(text);
+    }
+}
