7e5656de73bb12baa864705a81819969f0804e47
noko <nokok.kz@gmail.com>
2013-11-30 17:43:31 +0900

    Delete Log.java
    Logクラスの削除
    Loggerクラスの導入

--- src/main/java/net/nokok/twitduke/util/TwitterWrapper.java
+++ src/main/java/net/nokok/twitduke/util/TwitterWrapper.java
@@ -1,8 +1,13 @@
 package net.nokok.twitduke.util;
 
+import net.nokok.twitduke.util.config.DefaultConfig;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 /**
  * @author noko
@@ -10,11 +15,13 @@ import twitter4j.TwitterFactory;
  */
 public class TwitterWrapper {
 
-    private Twitter twitter;
+    private static Logger  logger;
+    private        Twitter twitter;
     private static final TwitterWrapper instance = new TwitterWrapper();
 
     private TwitterWrapper() {
         twitter = new TwitterFactory().getInstance();
+        logger = Logger.getLogger("TwitterWrapper Logger");
     }
 
     public static TwitterWrapper getInstance() {
