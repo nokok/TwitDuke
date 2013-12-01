77ff73ba9c621a87e88ff279b6682e40625a3056
nokok <nokok.kz@gmail.com>
2013-12-01 22:54:16 +0900

    再 設 計

--- src/main/java/net/nokok/twitduke/gui/TweetCell.java
+++
@@ -1,8 +0,0 @@
-package net.nokok.twitduke.gui;
-
-/**
- * @author noko
- *         ツイートを表示するセルです。
- */
-public class TweetCell extends ElementCell {
-}
--- src/main/java/net/nokok/twitduke/gui/UserCell.java
+++
@@ -1,8 +0,0 @@
-package net.nokok.twitduke.gui;
-
-/**
- * @author noko
- *         ユーザーを表示するセルです
- */
-public class UserCell extends ElementCell {
-}
