582b9f8b6cc840c26cad95ef33244da28b24e47c
nokok <nokok.kz@gmail.com>
2014-01-01 21:52:56 +0900

    あんふぁぼアクションを削除

--- src/main/java/net/nokok/twitduke/view/TweetPopupMenu.java
+++ src/main/java/net/nokok/twitduke/view/TweetPopupMenu.java
@@ -9,7 +9,6 @@ public class TweetPopupMenu extends JPopupMenu {
 
     private final TWMenuItem reply      = new TWMenuItem("リプライ");
     private final TWMenuItem favorite   = new TWMenuItem("お気に入り");
-    private final TWMenuItem unfavorite = new TWMenuItem("お気に入り解除");
     private final TWMenuItem retweet    = new TWMenuItem("リツイート");
     private final TWMenuItem openURL    = new TWMenuItem("URLを開く");
     private final TWMenuItem delete     = new TWMenuItem("削除");
@@ -19,7 +18,6 @@ public class TweetPopupMenu extends JPopupMenu {
         this.setBackground(DefaultColor.TWButton.DEFAULT_BACKGROUND);
         this.add(reply);
         this.add(favorite);
-        this.add(unfavorite);
         this.add(retweet);
         this.add(openURL);
         this.add(delete);
@@ -33,8 +31,6 @@ public class TweetPopupMenu extends JPopupMenu {
         return favorite;
     }
 
-    public TWMenuItem getUnfavorite() {
-        return unfavorite;
     }
 
     public TWMenuItem getRetweet() {
