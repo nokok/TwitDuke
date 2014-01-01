6653e92d3bec7708f5d08bc39480b06740702d84
nokok <nokok.kz@gmail.com>
2014-01-01 21:53:41 +0900

    右クリックメニューに検索を追加

--- src/main/java/net/nokok/twitduke/view/TweetPopupMenu.java
+++ src/main/java/net/nokok/twitduke/view/TweetPopupMenu.java
@@ -11,6 +11,7 @@ public class TweetPopupMenu extends JPopupMenu {
     private final TWMenuItem favorite   = new TWMenuItem("お気に入り");
     private final TWMenuItem retweet    = new TWMenuItem("リツイート");
     private final TWMenuItem openURL    = new TWMenuItem("URLを開く");
+    private final TWMenuItem search     = new TWMenuItem("Googleで検索");
     private final TWMenuItem delete     = new TWMenuItem("削除");
 
     public TweetPopupMenu() {
@@ -20,6 +21,7 @@ public class TweetPopupMenu extends JPopupMenu {
         this.add(favorite);
         this.add(retweet);
         this.add(openURL);
+        this.add(search);
         this.add(delete);
     }
 
