d57a3d2919987ee0f67c5552e5199760fbff1037
nokok <nokok.kz@gmail.com>
2014-02-14 01:21:16 +0900

    importミスを修正

--- src/main/java/net/nokok/twitduke/model/thread/FileCreateWatcher.java
+++ src/main/java/net/nokok/twitduke/model/thread/FileCreateWatcher.java
@@ -1,7 +1,7 @@
 package net.nokok.twitduke.model.thread;
 
 import java.io.File;
-import net.nokok.twitduke.util.Threads;
+import net.nokok.twitduke.util.ThreadUtil;
 
 /**
  * 指定したファイルが作成されるのを3秒間隔で監視し、作成されたらfilesCreatedが呼ばれます
