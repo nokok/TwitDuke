a62429ace17f9078f11b168a28a92ab2fc35c0c3
nokok <nokok.kz@gmail.com>
2014-04-18 16:17:16 +0900

    first commit

---
+++ TwitDuke/src/main/java/net/nokok/twitduke/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.nokok.twitduke;
+
+/**
+ *
+ * @author noko
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hoge");
+    }
+}
