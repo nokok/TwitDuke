8e8c88a55acd6c2a4b0a10e2948211537c8c0c3b
nokok <nokok.kz@gmail.com>
2014-04-26 01:29:18 +0900

    LicensePanelクラスを追加

---
+++ TwitDuke-components/src/main/java/net/nokok/twitduke/component/basic/TWTextArea.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 noko <nokok.kz at gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.nokok.twitduke.component.basic;
+
+import java.awt.Color;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author noko <nokok.kz at gmail.com>
+ */
+public class TWTextArea extends JTextArea {
+
+    private static final long serialVersionUID = 3723800077851443280L;
+
+    public static final Color BACKGROUND_COLOR = new Color(40, 40, 40);
+    public static final Color FOREGROUND_COLOR = new Color(200, 200, 200);
+
+    public TWTextArea() {
+        setBackground(BACKGROUND_COLOR);
+        setForeground(FOREGROUND_COLOR);
+    }
+
+    public static JTextArea newEditableTextArea() {
+        JTextArea textArea = new TWTextArea();
+        return textArea;
+    }
+
+    public static JTextArea newTextArea(String text) {
+        JTextArea textArea = new TWTextArea();
+        textArea.setText(text);
+        textArea.setEditable(false);
+        return textArea;
+    }
+}
