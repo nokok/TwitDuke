d3dc8a67ec90b9b809fa2f2a7f46f787695fa847
nokok <nokok.kz@gmail.com>
2014-04-30 20:45:50 +0900

    分割していたモジュールを一つにした

---
+++ src/main/java/net/nokok/twitduke/components/basic/TWButton.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 noko <nokok.kz at gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.nokok.twitduke.components.basic;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+
+/**
+ * TwitDuke用のカスタマイズ済みJButtonを定義します
+ * <p>
+ * @author noko <nokok.kz at gmail.com>
+ */
+public class TWButton extends JButton {
+
+    private static final long serialVersionUID = -6950374784491086356L;
+
+    /**
+     * ボタンの背景色です。
+     */
+    public static final Color BACKGROUND_COLOR = new Color(189, 195, 199);
+    /**
+     * ボタンの前景色(フォント色)です。
+     */
+    public static final Color FOREGROUND_COLOR = new Color(40, 40, 40);
+
+    public TWButton() {
+        setBackground(BACKGROUND_COLOR);
+        setForeground(FOREGROUND_COLOR);
+        setOpaque(true);
+        setBorderPainted(false);
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setBackground(getBackground().darker());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                setBackground(getBackground().brighter());
+            }
+        });
+    }
+
+    public TWButton(String text) {
+        this();
+        setText(text);
+    }
+}
---
+++ src/main/java/net/nokok/twitduke/view/OAuthDialog.java
@@ -0,0 +1,92 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 noko <nokok.kz at gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.nokok.twitduke.view;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import net.nokok.twitduke.components.OKCancelButtonPanel;
+import net.nokok.twitduke.components.basic.TWFrame;
+import net.nokok.twitduke.components.basic.TWLabel;
+import net.nokok.twitduke.components.basic.TWPanel;
+import net.nokok.twitduke.components.basic.TWTextField;
+import net.nokok.twitduke.core.api.view.Dialog;
+import net.nokok.twitduke.core.api.view.DialogResultListener;
+
+/**
+ * OAuthダイアログはOAuth認証時に表示されるPINの入力画面を担当するクラスです。
+ * このクラスはあくまでViewのみでPIN入力関連の実際の処理はDialogResultListener<Integer>実装クラスで
+ * 行われます。
+ * <p>
+ * @author noko <nokok.kz at gmail.com>
+ */
+public class OAuthDialog implements Dialog<String> {
+
+    private final JFrame frame;
+    private final OKCancelButtonPanel okCancelPanel = new OKCancelButtonPanel();
+    private DialogResultListener<String> dialogResultListener;
+
+    /**
+     * OAuth認証のPINを入力するダイアログを生成します。
+     */
+    public OAuthDialog() {
+        frame = new TWFrame("認証して下さい");
+        JTextField textField = new TWTextField();
+        textField.setHorizontalAlignment(JTextField.CENTER);
+        frame.setLayout(new BorderLayout());
+        frame.add(createNorthPanel(), BorderLayout.NORTH);
+        frame.add(textField, BorderLayout.CENTER);
+        frame.add(okCancelPanel, BorderLayout.SOUTH);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+
+        okCancelPanel.addOKButtonAction(e -> {
+            dialogResultListener.okButtonPushed(textField.getText());
+        });
+        okCancelPanel.addCancelButtonAction(e
+                -> dialogResultListener.cancelButtonPushed());
+    }
+
+    private JPanel createNorthPanel() {
+        JPanel panel = new TWPanel();
+        panel.add(new TWLabel("ログイン後、表示された数字を半角数字で入力して下さい"));
+        return panel;
+    }
+
+    @Override
+    public void dispose() {
+        frame.dispose();
+    }
+
+    @Override
+    public void setDialogResultListener(DialogResultListener<String> dialogResultListener) {
+        this.dialogResultListener = dialogResultListener;
+    }
+
+    @Override
+    public void show() {
+        frame.setVisible(true);
+    }
+}
