d0d1fb015292d2e383fc9ddb7627e4b2f3043ec2
nokok <nokok.kz@gmail.com>
2014-05-05 23:06:54 +0900

    UncheckedScriptExceptionクラスにJavadocを追加

--- src/main/java/net/nokok/twitduke/core/type/UncheckedScriptException.java
+++ src/main/java/net/nokok/twitduke/core/type/UncheckedScriptException.java
@@ -6,15 +6,27 @@
 package net.nokok.twitduke.core.type;
 
 /**
+ * ScriptExceptionを実行時例外にラップしたクラスです
+ * RuntimeExceptionの仕様及び制限に依存します
  *
  * @author noko < nokok.kz at gmail.com >
  */
 public class UncheckedScriptException extends RuntimeException {
 
+    /**
+     * 指定された原因を使用して新しいスクリプトの実行時例外を生成します。
+     *
+     * @param e 原因
+     */
     public UncheckedScriptException(Exception e) {
         super(e);
     }
 
+    /**
+     * 指定された詳細メッセージを使用して新しいスクリプトの実行時例外を生成します
+     *
+     * @param s 詳細メッセージ
+     */
     public UncheckedScriptException(String s) {
         super(s);
     }
