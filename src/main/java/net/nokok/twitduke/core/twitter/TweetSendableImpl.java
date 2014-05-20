58d10b6eccbed8ba5943c36ea50cae12ded06c83
nokok <nokok.kz@gmail.com>
2014-05-21 02:05:06 +0900

    TweetSendableImplクラスを追加

---
+++ src/main/java/net/nokok/twitduke/core/twitter/TweetSendableImpl.java
@@ -0,0 +1,48 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 noko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.nokok.twitduke.core.twitter;
+
+import net.nokok.twitduke.core.type.Tweet;
+import twitter4j.AsyncTwitter;
+import twitter4j.auth.AccessToken;
+
+public class TweetSendableImpl implements TweetSendable {
+
+    private final AsyncTwitter asyncTwitter;
+
+    public TweetSendableImpl(AccessToken accessToken) {
+        asyncTwitter = new AsyncTwitterInstanceGeneratorImpl().generate(accessToken);
+    }
+
+    @Override
+    public void sendTweet(Tweet tweet) {
+        asyncTwitter.updateStatus(tweet.get());
+    }
+
+    @Override
+    public void sendTweet(Tweet tweet, Footer footer) {
+        asyncTwitter.updateStatus(tweet.applyFooter(footer).get());
+    }
+
+}
