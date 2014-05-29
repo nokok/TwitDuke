c7cdf698f35b08febe08d8f4c74e2462153e46ad
nokok <nokok.kz@gmail.com>
2014-05-30 00:10:59 +0900

    ScrollableTimelinePanelクラスを追加

---
+++ src/main/java/net/nokok/twitduke/components/ScrollableTimelinePanel.java
@@ -0,0 +1,49 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 noko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.nokok.twitduke.components;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import net.nokok.twitduke.components.basic.TWScrollPane;
+
+public class ScrollableTimelinePanel {
+
+    private final JPanel timelinePanel;
+    private final JScrollPane scrollPane;
+
+    public ScrollableTimelinePanel() {
+        timelinePanel = new TimelinePanel();
+        scrollPane = new TWScrollPane(timelinePanel);
+    }
+
+    public void addComponent(JComponent component) {
+        timelinePanel.add(component);
+    }
+
+    public JScrollPane getScrollPane() {
+        return scrollPane;
+    }
+
+}
