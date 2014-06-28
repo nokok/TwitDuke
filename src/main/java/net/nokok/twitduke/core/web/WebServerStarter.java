22d18a64a215dd5b24727a3e1c9be4f0bae62572
nokok <nokok.kz@gmail.com>
2014-06-28 15:15:29 +0900

    BurnOwenHandlerをWebServerStarterに登録

--- src/main/java/net/nokok/twitduke/core/web/WebServerStarter.java
+++ src/main/java/net/nokok/twitduke/core/web/WebServerStarter.java
@@ -24,6 +24,7 @@
 package net.nokok.twitduke.core.web;
 
 import net.nokok.twitduke.core.web.handlers.AddAccountHandler;
+import net.nokok.twitduke.core.web.handlers.BurnOwenHandler;
 import net.nokok.twitduke.core.web.handlers.SendTweetHandler;
 import net.nokok.twitduke.core.web.handlers.ShindanmakerHandler;
 import net.nokok.twitduke.core.web.handlers.TweetWithFooterHandler;
@@ -45,6 +46,7 @@ public class WebServerStarter implements Runnable {
                 .addHandler(new ShindanmakerHandler(accessToken).getHandler())
                 .addHandler(new TweetWithFooterHandler(accessToken).getHandler())
                 .addHandlerRetrievable(AddAccountHandler.class)
+                .addHandler(new BurnOwenHandler(accessToken).get())
                 .call();
     }
 
