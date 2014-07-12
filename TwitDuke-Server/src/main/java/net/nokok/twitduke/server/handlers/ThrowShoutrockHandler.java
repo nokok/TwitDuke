542d55d1ae64369b1a2a2bcf6d136d90c25fe192
Tomona Nanase <nanase@users.noreply.github.com>
2014-07-12 23:05:35 +0900

    しゃうさぎ氏を1mから10000mまで投げられるよう修正
    SecureRandom#nextIntの実装により、9999mまでしか投げられないため+1mを単純に加算。

--- TwitDuke-Server/src/main/java/net/nokok/twitduke/server/handlers/ThrowShoutrockHandler.java
+++ TwitDuke-Server/src/main/java/net/nokok/twitduke/server/handlers/ThrowShoutrockHandler.java
@@ -41,7 +41,7 @@ public class ThrowShoutrockHandler implements Retrievable<Handler> {
 
         @Override
         public void doHandle(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-            asyncTwitter.updateStatus("しゃうさぎ氏を" + new SecureRandom().nextInt(10000) + "m飛ばしました #TwitDuke");
+            asyncTwitter.updateStatus("しゃうさぎ氏を" + (new SecureRandom().nextInt(10000) + 1) + "m飛ばしました #TwitDuke");
             sendOK();
         }
 
