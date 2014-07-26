9b156bee139fe26f9d6707c022251f1c52b1cf7d
noko <nokok.kz@gmail.com>
2014-07-27 00:23:26 +0900

    FXMLResourcesクラスを追加

---
+++ TwitDuke-Resources/src/main/java/net/nokok/twitduke/resources/FXMLResources.java
@@ -0,0 +1,35 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 noko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.nokok.twitduke.resources;
+
+import com.google.common.io.Resources;
+import java.io.File;
+import java.net.URL;
+import java.util.Optional;
+
+public class FXMLResources {
+
+    public static final Optional<URL> MAIN_FXML = Optional.ofNullable(Resources.getResource(String.join(File.separator, "fxml", "main.fxml")));
+    public static final Optional<URL> TWEETCELL_FXML = Optional.ofNullable(Resources.getResource(String.join(File.separator, "fxml", "tweetcell.fxml")));
+}
