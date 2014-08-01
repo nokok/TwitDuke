53c6ae49f3d5c80c41ae0bc79ebf2d96cc87e55a
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2014-08-02 00:58:27 +0900

    pasteを実装

--- TwitDuke-Components/src/main/java/net/nokok/twitduke/components/actions/Action_Paste.java
+++ TwitDuke-Components/src/main/java/net/nokok/twitduke/components/actions/Action_Paste.java
@@ -6,6 +6,8 @@
 package net.nokok.twitduke.components.actions;
 
 import javafx.event.EventHandler;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
 import javafx.scene.input.KeyEvent;
 
 /**
@@ -16,6 +18,21 @@ public class Action_Paste implements EventHandler<KeyEvent> {
 
     @Override
     public void handle(KeyEvent event) {
-        System.out.println("Event:" + this.getClass().getCanonicalName() + "[" + event.getCode() + "]");
+        if ( !(event.getSource() instanceof TextArea) ) {
+            return;
+        }
+        String buffer = Clipboard.getSystemClipboard().getString();
+        if ( buffer.isEmpty() ) {
+            return;
+        }
+        TextArea src = (TextArea) event.getSource();
+        String txt = src.getText();
+        int selectionStart = src.getSelection().getStart();
+        int selectionEnd = src.getSelection().getEnd();
+        StringBuilder sb = new StringBuilder(src.getText());
+        sb.delete(selectionStart, selectionEnd);
+        sb.insert(selectionStart, buffer);
+        src.setText(sb.toString());
+        src.selectRange(selectionStart + buffer.length(), selectionStart + buffer.length());
     }
 }
