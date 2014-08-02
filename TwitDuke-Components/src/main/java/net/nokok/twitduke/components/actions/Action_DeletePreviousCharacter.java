49b10fc5752c0a9f1340f680d8150d86df7b934f
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2014-08-02 13:17:22 +0900

    ctl+d と ctrl+h で前後の文字削除をできるようにした

---
+++ TwitDuke-Components/src/main/java/net/nokok/twitduke/components/actions/Action_DeletePreviousCharacter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.nokok.twitduke.components.actions;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+
+/**
+ *
+ * @author wtnbsts
+ */
+public class Action_DeletePreviousCharacter implements EventHandler<KeyEvent> {
+
+    @Override
+    public void handle(KeyEvent event) {
+        if ( !(event.getSource() instanceof TextArea) ) {
+            return;
+        }
+        final TextArea src = (TextArea) event.getSource();
+        src.deletePreviousChar();
+    }
+}
