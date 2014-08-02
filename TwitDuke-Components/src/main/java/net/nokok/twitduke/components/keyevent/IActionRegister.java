40d0807ee43ad65872ab5750a93d4a66ac63d978
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2014-08-03 04:27:05 +0900

    Merge branch 'pr0'
    Conflicts:
    	TwitDuke-Components/src/main/java/net/nokok/twitduke/components/keyevent/KeyBind.java
    	TwitDuke-Test/src/test/java/net/nokok/twitduke/components/keyevent/KeyBindTest.java

--- TwitDuke-Components/src/main/java/net/nokok/twitduke/components/keyevent/IActionRegister.java
+++ TwitDuke-Components/src/main/java/net/nokok/twitduke/components/keyevent/IActionRegister.java
@@ -1,6 +1,5 @@
 package net.nokok.twitduke.components.keyevent;
 
-import java.awt.Component;
 import java.util.List;
 import javafx.scene.Node;
 
@@ -8,20 +7,6 @@ import javafx.scene.Node;
  * Created by wtnbsts on 2014/07/26.
  */
 public interface IActionRegister {
-
-    /**
-     * キーボードショートカットを設定するノードを指定して、キーボードショートカット設定インスタンスを生成する
-     *
-     * @param root キーボードを設定したいノード
-     *
-     * @return ショートカット設定インスタンス
-     *
-     * @deprecated swing用クラスの削除と同時に消されます
-     */
-    @Deprecated
-    static IActionRegister newInstance(final Component root) {
-        return new JComponentActionRegister(root);
-    }
 
     /**
      * キーボードショートカットを設定するノードを指定して、キーボードショートカット設定インスタンスを生成する
