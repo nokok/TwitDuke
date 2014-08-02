c7f64265bcbaa7f94bef642eeb1733934e34540e
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2014-08-03 03:09:42 +0900

    javadoc記述

--- TwitDuke-Components/src/main/java/net/nokok/twitduke/components/actions/ActionUtil.java
+++ TwitDuke-Components/src/main/java/net/nokok/twitduke/components/actions/ActionUtil.java
@@ -11,17 +11,41 @@ package net.nokok.twitduke.components.actions;
  */
 public class ActionUtil {
 
+    /**
+     * 文字列と、その文字列中で選択されている場所を渡すと、その選択されている場所の行末の番号を計算する
+     *
+     * @param txt           文字列
+     * @param caretPosition 文字列中で選択されている場所
+     *
+     * @return 行末の番号
+     */
     public static int getEndOfLine(final String txt, final int caretPosition) {
         final int lineSepPosition = txt.indexOf(System.lineSeparator(), caretPosition);
         return lineSepPosition < 0 ? txt.length() : lineSepPosition;
     }
 
+    /**
+     * 文字列と、その文字列中で選択されている場所を渡すと、その選択されている場所の行頭の番号を計算する
+     *
+     * @param txt           文字列
+     * @param caretPosition 文字列中で選択されている場所
+     *
+     * @return 行頭の番号
+     */
     public static int getBeginningOfLine(final String txt, final int caretPosition) {
         final String subtext = txt.substring(0, caretPosition);
         final int lineSepPosition = subtext.lastIndexOf(System.lineSeparator(), caretPosition);
         return lineSepPosition < 0 ? 0 : lineSepPosition + System.lineSeparator().length();
     }
 
+    /**
+     * 文字列と、その文字列中で選択されている場所を渡すと、その選択されている場所の上にある文字の番号を計算する
+     *
+     * @param txt           文字列
+     * @param caretPosition 文字列中で選択されている場所
+     *
+     * @return 上にある文字の番号
+     */
     public static int getUpSide(final String txt, final int caretPosition) {
         final int lineStartPosit = getBeginningOfLine(txt, caretPosition);
         if ( lineStartPosit == 0 ) {
@@ -38,6 +62,14 @@ public class ActionUtil {
         }
     }
 
+    /**
+     * 文字列と、その文字列中で選択されている場所を渡すと、その選択されている場所の下にある文字の番号を計算する
+     *
+     * @param txt           文字列
+     * @param caretPosition 文字列中で選択されている場所
+     *
+     * @return 下にある文字の番号
+     */
     public static int getDownSide(final String txt, final int caretPosition) {
         final int lineEndPosit = getEndOfLine(txt, caretPosition);
         if ( lineEndPosit == txt.length() ) {
--- TwitDuke-Components/src/main/java/net/nokok/twitduke/components/keyevent/IActionRegister.java
+++ TwitDuke-Components/src/main/java/net/nokok/twitduke/components/keyevent/IActionRegister.java
@@ -9,10 +9,27 @@ import javafx.scene.Node;
  */
 public interface IActionRegister {
 
+    /**
+     * キーボードショートカットを設定するノードを指定して、キーボードショートカット設定インスタンスを生成する
+     *
+     * @param root キーボードを設定したいノード
+     *
+     * @return ショートカット設定インスタンス
+     *
+     * @deprecated swing用クラスの削除と同時に消されます
+     */
+    @Deprecated
     static IActionRegister newInstance(final Component root) {
         return new JComponentActionRegister(root);
     }
 
+    /**
+     * キーボードショートカットを設定するノードを指定して、キーボードショートカット設定インスタンスを生成する
+     *
+     * @param root キーボードを設定したいノード
+     *
+     * @return ショートカット設定インスタンス
+     */
     static IActionRegister newInstance(final Node root) {
         return new JavaFXActionRegister(root);
     }
