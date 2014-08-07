85152636434c0e129dfec13261393bd5ae6a0f6a
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2014-08-07 22:50:18 +0900

    KeyBindクラスをイミュータブル化

--- TwitDuke-Components/src/main/java/net/nokok/twitduke/components/keyevent/KeyBind.java
+++ TwitDuke-Components/src/main/java/net/nokok/twitduke/components/keyevent/KeyBind.java
@@ -1,6 +1,7 @@
 package net.nokok.twitduke.components.keyevent;
 
-import java.util.function.Consumer;
+import java.util.Objects;
+import javafx.scene.input.KeyCombination;
 
 /**
  * Created by wtnbsts on 2014/07/23.
@@ -11,33 +12,32 @@ public class KeyBind implements Comparable<KeyBind> {
      * キー入力の文字列表現
      * 例) ctrl+a
      */
-    private String keyStroke;
+    private KeyCombination keyStroke;
     /**
      * キーボードショートカットが有効になるコンポーネントの条件 (現在はクラス名の完全一致)
      * 例) net.nokok.twitduke.components.TWButton
      */
     private String selector;
+
     /**
-     * コンポーネントから、キーボードショートカットを無効化する為の処理
+     * キーボードショートカットのキー設定と、
+     * ショートカットが有効になるコンポーネントの条件を取得してインスタンスを生成する
+     *
+     * @param shortcutKeyBind ショートカットキー
+     * @param selector        キーボードショートカットが有効になるコンポーネントの条件
      */
-    private Consumer<KeyBind> removeFunc;
+    public KeyBind(final KeyCombination shortcutKeyBind, final String selector) {
+        this.keyStroke = shortcutKeyBind;
+        this.selector = selector;
+    }
 
     /**
      * キー入力の文字列表現を取得する
      *
      * @return キー入力の文字列表現
      */
-    public String getKeyStroke() {
+    public KeyCombination getKeyStroke() {
         return keyStroke;
-    }
-
-    /**
-     * キー入力の文字列表現を設定する
-     *
-     * @param keyStroke キー入力の文字列表現
-     */
-    public void setKeyStroke(final String keyStroke) {
-        this.keyStroke = keyStroke;
     }
 
     /**
@@ -49,61 +49,30 @@ public class KeyBind implements Comparable<KeyBind> {
         return selector;
     }
 
-    /**
-     * キーボードショートカットが有効になるコンポーネントの条件を取得する
-     *
-     * @param selector キーボードショートカットが有効になるコンポーネントの条件
-     */
-    public void setSelector(final String selector) {
-        this.selector = selector;
-    }
-
-    /**
-     * コンポーネントから、キーボードショートカットを無効化する為の処理を設定する
-     *
-     * @param removeFunc コンポーネントから、キーボードショートカットを無効化する為の処理
-     */
-    public void setRemoveFunc(final Consumer<KeyBind> removeFunc) {
-        this.removeFunc = removeFunc;
-    }
-
-    /**
-     * コンポーネントから、キーボードショートカットを無効化する為の処理を実行する
-     */
-    public void remove() {
-        if ( removeFunc != null ) {
-            removeFunc.accept(this);
-        }
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if ( this == o ) {
-            return true;
-        }
-        if ( o == null || getClass() != o.getClass() ) {
-            return false;
-        }
-
-        KeyBind keyBind = (KeyBind) o;
-
-        if ( keyStroke != null ? !keyStroke.equals(keyBind.keyStroke) : keyBind.keyStroke != null ) {
-            return false;
-        }
-        if ( removeFunc != null ? !removeFunc.equals(keyBind.removeFunc) : keyBind.removeFunc != null ) {
-            return false;
-        }
-        return !(selector != null ? !selector.equals(keyBind.selector)
-                 : keyBind.selector != null);
-
-    }
-
     @Override
     public int hashCode() {
-        int result = keyStroke != null ? keyStroke.hashCode() : 0;
-        result = 31 * result + (selector != null ? selector.hashCode() : 0);
-        result = 31 * result + (removeFunc != null ? removeFunc.hashCode() : 0);
-        return result;
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.keyStroke);
+        hash = 11 * hash + Objects.hashCode(this.selector);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( obj == null ) {
+            return false;
+        }
+        if ( getClass() != obj.getClass() ) {
+            return false;
+        }
+        final KeyBind other = (KeyBind) obj;
+        if ( !Objects.equals(this.keyStroke, other.keyStroke) ) {
+            return false;
+        }
+        if ( !Objects.equals(this.selector, other.selector) ) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -119,17 +88,11 @@ public class KeyBind implements Comparable<KeyBind> {
             return namedif;
         }
 
-        int keydif = (keyStroke == null ? "" : keyStroke).compareTo(
-                keyBind.keyStroke == null ? "" : keyBind.keyStroke
+        int keydif = (keyStroke == null ? "" : keyStroke.getName()).compareTo(
+                keyBind.keyStroke == null ? "" : keyBind.keyStroke.getName()
         );
         if ( keydif != 0 ) {
             return keydif;
-        }
-
-        if ( removeFunc == null && keyBind.removeFunc != null ) {
-            return -1;
-        } else if ( removeFunc != null && keyBind.removeFunc == null ) {
-            return 1;
         }
 
         return 0;
--- TwitDuke-Test/src/test/java/net/nokok/twitduke/components/keyevent/KeyBindTest.java
+++ TwitDuke-Test/src/test/java/net/nokok/twitduke/components/keyevent/KeyBindTest.java
@@ -1,14 +1,10 @@
 package net.nokok.twitduke.components.keyevent;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
+import javafx.scene.input.KeyCombination;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -18,14 +14,6 @@ public class KeyBindTest {
 
     private static final String fnKeyStroke = "keyStroke";
     private static final String fnComponentName = "selector";
-    private static final String fnRemoveFunc = "removeFunc";
-
-    private KeyBind bind;
-
-    @Before
-    public void setUp() {
-        bind = new KeyBind();
-    }
 
     /**
      * コンストラクタ
@@ -34,10 +22,9 @@ public class KeyBindTest {
      */
     @Test
     public void testConstructor() throws Exception {
-        assertNull(getStringValue(fnKeyStroke));
-        assertNull(getStringValue(fnComponentName));
-
-        assertNull(getValue(fnRemoveFunc));
+        KeyBind bind = new KeyBind(null, null);
+        assertNull(getStringValue(bind, fnKeyStroke));
+        assertNull(getStringValue(bind, fnComponentName));
     }
 
     /**
@@ -46,22 +33,9 @@ public class KeyBindTest {
      */
     @Test
     public void testGetKeyStroke() throws Exception {
-        String value = "ctrl K";
-        bind.setKeyStroke(value);
-        assertEquals(value, getStringValue(fnKeyStroke));
-    }
-
-    /**
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testSetKeyStroke() throws Exception {
-        String value = "ctrl K";
-        bind.setKeyStroke(value);
-        assertEquals(value, getStringValue(fnKeyStroke));
-        bind.setKeyStroke(null);
-        assertEquals(null, getStringValue(fnKeyStroke));
+        String value = "ctrl+K";
+        KeyBind bind = new KeyBind(KeyCombination.keyCombination(value), null);
+        assertEquals(KeyCombination.keyCombination(value), getValue(bind, fnKeyStroke));
     }
 
     /**
@@ -71,55 +45,15 @@ public class KeyBindTest {
     @Test
     public void testGetSelector() throws Exception {
         String value = "AnyComponent";
-        bind.setSelector(value);
+        KeyBind bind = new KeyBind(null, value);
         assertEquals(value, bind.getSelector());
-        bind.setSelector(null);
-        assertEquals(null, bind.getSelector());
-    }
-
-    /**
-     *
-     * @throws java.lang.Exception
-     */
-    @Test
-    public void testSetSelector() throws Exception {
-        String value = "AnyComponent";
-        bind.setSelector(value);
-        assertEquals(value, getStringValue(fnComponentName));
-        bind.setSelector(null);
-        assertEquals(null, getStringValue(fnComponentName));
-    }
-
-    /**
-     *
-     * @throws java.lang.Exception
-     */
-    @Test
-    public void testSetRemoveFunc() throws Exception {
-        Consumer<KeyBind> value = System.out::println;
-        bind.setRemoveFunc(value);
-        assertEquals(value, getValue(fnRemoveFunc));
-        bind.setRemoveFunc(null);
-        assertEquals(null, getValue(fnRemoveFunc));
-    }
-
-    /**
-     *
-     * @throws java.lang.Exception
-     */
-    @Test
-    public void testRemove() throws Exception {
-        List<KeyBind> buf = new ArrayList<>();
-        bind.setRemoveFunc(buf::add);
-        bind.remove();
-        assertEquals(1, buf.size());
     }
 
     @Test
     public void testHashCode() throws Exception {
-        assertEquals(bind.hashCode(), 0);
-        bind.setKeyStroke("Any");
-        assertNotSame(bind.hashCode(), 0);
+        KeyBind bind1 = new KeyBind(null, null);
+        KeyBind bind2 = new KeyBind(KeyCombination.keyCombination("ctrl+k"), null);
+        assertTrue(bind1.hashCode() != bind2.hashCode());
     }
 
     /**
@@ -129,6 +63,7 @@ public class KeyBindTest {
      */
     @Test
     public void testEqualsT0() throws Exception {
+        KeyBind bind = new KeyBind(KeyCombination.keyCombination("ctrl+k"), "any");
         assertTrue(bind.equals(bind));
     }
 
@@ -139,8 +74,8 @@ public class KeyBindTest {
      */
     @Test
     public void testEqualsT1() throws Exception {
-        KeyBind b1 = new KeyBind();
-        KeyBind b2 = new KeyBind();
+        KeyBind b1 = new KeyBind(null, null);
+        KeyBind b2 = new KeyBind(null, null);
         assertTrue(b1.hashCode() == b2.hashCode());
         assertTrue(b1.equals(b2));
         assertTrue(b2.equals(b1));
@@ -153,15 +88,8 @@ public class KeyBindTest {
      */
     @Test
     public void testEqualsT2() throws Exception {
-        KeyBind b1 = new KeyBind();
-        KeyBind b2 = new KeyBind();
-        b1.setKeyStroke("ctrl K");
-        b2.setKeyStroke("ctrl K");
-        b1.setSelector("Any");
-        b2.setSelector("Any");
-        Consumer<KeyBind> func = b1::equals;
-        b1.setRemoveFunc(func);
-        b2.setRemoveFunc(func);
+        KeyBind b1 = new KeyBind(KeyCombination.keyCombination("alt+k"), "any");
+        KeyBind b2 = new KeyBind(KeyCombination.keyCombination("alt+k"), "any");
         assertTrue(b1.hashCode() == b2.hashCode());
         assertTrue(b1.equals(b2));
         assertTrue(b2.equals(b1));
@@ -174,10 +102,8 @@ public class KeyBindTest {
      */
     @Test
     public void testEqualsF0() throws Exception {
-        KeyBind b1 = new KeyBind();
-        KeyBind b2 = new KeyBind();
-        b1.setKeyStroke("ctrl K");
-        b2.setKeyStroke("");
+        KeyBind b1 = new KeyBind(KeyCombination.keyCombination("alt+k"), "any");
+        KeyBind b2 = new KeyBind(KeyCombination.keyCombination("k"), "any");
         assertTrue(!b1.equals(b2));
         assertTrue(!b2.equals(b1));
     }
@@ -189,47 +115,30 @@ public class KeyBindTest {
      */
     @Test
     public void testEqualsF1() throws Exception {
-        KeyBind b1 = new KeyBind();
-        KeyBind b2 = new KeyBind();
-        b1.setSelector("Any");
-        b2.setSelector("Other");
+        KeyBind b1 = new KeyBind(KeyCombination.keyCombination("alt+k"), "any");
+        KeyBind b2 = new KeyBind(KeyCombination.keyCombination("alt+k"), "other");
         assertTrue(!b1.equals(b2));
         assertTrue(!b2.equals(b1));
     }
 
-    /**
-     * コールバックが違うときの not equal
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testEqualsF3() throws Exception {
-        KeyBind b1 = new KeyBind();
-        KeyBind b2 = new KeyBind();
-        b1.setRemoveFunc(b1::equals);
-        b2.setRemoveFunc(b2::equals);
-        assertTrue(!b1.equals(b2));
-        assertTrue(!b2.equals(b1));
-    }
-
-    private Field getField(final String fieldName) throws NoSuchFieldException {
+    private Field getField(final KeyBind bind, final String fieldName) throws NoSuchFieldException {
         Field field = bind.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);
         return field;
     }
 
-    private Object getValue(final String fieldName)
+    private Object getValue(final KeyBind bind, final String fieldName)
             throws NoSuchFieldException, IllegalAccessException {
-        return getField(fieldName).get(bind);
+        return getField(bind, fieldName).get(bind);
     }
 
-    private String getStringValue(final String fieldName)
+    private String getStringValue(final KeyBind bind, final String fieldName)
             throws NoSuchFieldException, IllegalAccessException {
-        return (String) getValue(fieldName);
+        return (String) getValue(bind, fieldName);
     }
 
-    private int getIntValue(final String fieldName)
+    private int getIntValue(final KeyBind bind, final String fieldName)
             throws NoSuchFieldException, IllegalAccessException {
-        return getField(fieldName).getInt(bind);
+        return getField(bind, fieldName).getInt(bind);
     }
 }
