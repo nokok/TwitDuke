b0d3bdee788c951eb4b1a3ba2466a690ee69de72
nokok <nokok.kz@gmail.com>
2014-08-30 16:16:35 +0900

    Main.javaの処理構造を変更

--- TwitDuke-Main/src/main/java/net/nokok/twitduke/Main.java
+++ TwitDuke-Main/src/main/java/net/nokok/twitduke/Main.java
@@ -41,13 +41,13 @@ import net.nokok.twitduke.components.keyevent.KeyMapSetting;
 import net.nokok.twitduke.components.keyevent.KeyMapStore;
 import net.nokok.twitduke.components.keyevent.KeyMapStoreBuilder;
 import net.nokok.twitduke.core.account.AccountManager;
+import net.nokok.twitduke.core.account.AccountManagerFactory;
 import net.nokok.twitduke.core.auth.LambdaOAuthFactory;
 import net.nokok.twitduke.core.auth.OAuthOnSuccess;
 import net.nokok.twitduke.core.auth.OAuthRunnable;
 import net.nokok.twitduke.core.log.ErrorLogExporter;
 import net.nokok.twitduke.core.twitter.TwitterNotificationListener;
 import net.nokok.twitduke.core.twitter.TwitterStreamRunner;
-import net.nokok.twitduke.core.view.Window;
 import net.nokok.twitduke.pluginsupport.PluginManager;
 import net.nokok.twitduke.pluginsupport.eventrunner.StreamEventRunner;
 import net.nokok.twitduke.resources.FXMLResources;
@@ -65,29 +65,38 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Stage configuredStage = configureStage(stage);
-        configuredStage.show();
+        AccountManager accountManager = AccountManagerFactory.newInstance();
+        if ( accountManager.hasValidAccount() ) {
+            FXMLLoader mainLoader = FXMLResources.MAIN.loader();
+            FXMLLoader toolbarLoader = FXMLResources.TWEET_TEXTAREA_TOOLBAR.loader();
+            FXMLLoader textAreaLoader = FXMLResources.TWEET_TEXTAREA.loader();
+            Scene main = new Scene(mainLoader.load());
+            MainViewController mainController = mainLoader.getController();
+            BorderPane borderPane = toolbarLoader.load();
+            TextArea textArea = textAreaLoader.load();
+            mainController.setTweetTextAreaToolbar(borderPane);
+            mainController.setTweetTextArea(textArea);
+            TweetTextareaToolbarController toolbarController = toolbarLoader.getController();
+            TweetTextareaController tweetTextareaController = textAreaLoader.getController();
+
+            toolbarController.addTweetTextAreaController(tweetTextareaController);
+            toolbarController.setSaveDraftButtonListener(tweetTextareaController);
+
+            applyKeymap(stage);
+
+            stage.setScene(main);
+
+            TwitterStreamRunner streamRunner = new TwitterStreamRunner(accountManager.readPrimaryAccount().get());
+            streamRunner.addHomeTimelineTweetCellListener(mainController);
+            streamRunner.run();
+            stage.show();
+        } else {
+            startOAuth(accountManager, System.out::println);
+        }
     }
 
     private Stage configureStage(Stage stage) throws Exception {
-        FXMLLoader mainLoader = FXMLResources.MAIN.loader();
-        FXMLLoader toolbarLoader = FXMLResources.TWEET_TEXTAREA_TOOLBAR.loader();
-        FXMLLoader textAreaLoader = FXMLResources.TWEET_TEXTAREA.loader();
-        Scene main = new Scene(mainLoader.load());
-        MainViewController mainController = mainLoader.getController();
-        BorderPane borderPane = toolbarLoader.load();
-        TextArea textArea = textAreaLoader.load();
-        mainController.setTweetTextAreaToolbar(borderPane);
-        mainController.setTweetTextArea(textArea);
-        TweetTextareaToolbarController toolbarController = toolbarLoader.getController();
-        TweetTextareaController tweetTextareaController = textAreaLoader.getController();
 
-        toolbarController.addTweetTextAreaController(tweetTextareaController);
-        toolbarController.setSaveDraftButtonListener(tweetTextareaController);
-
-        applyKeymap(stage);
-
-        stage.setScene(main);
         return stage;
     }
 
@@ -131,13 +140,11 @@ public class Main extends Application {
      * @param accountManager
      */
     private static void openWindow(AccountManager accountManager) {
-        Window window = Window.createNewWindow(accountManager);
         AccessToken accessToken = accountManager.readPrimaryAccount().orElseThrow(IllegalStateException::new);
         PluginManager globaPluginManager = new PluginManager("plugins", accessToken);
         TwitterStreamRunner streamRunner = new TwitterStreamRunner(accessToken);
         streamRunner.addTwitterListener(new StreamEventRunner(globaPluginManager.getPlugins()));
         streamRunner.addTwitterListener(new TwitterNotificationListener());
-        streamRunner.addHomeTimelineTweetCellListener(window);
         streamRunner.enableStackTrace();
         streamRunner.run();
     }
